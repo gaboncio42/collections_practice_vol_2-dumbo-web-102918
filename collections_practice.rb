69e05fbf36588eadb9ad5c464421ff4893c51c43
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-19 21:58:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,7 +26,7 @@ def merge_data(keys, data)
   counter = 0
   
   while (counter < keys.length)
-    merged_data.push()
+    merged_data.push(keys[counter])
 
 def find_cool(array)
   array.select do |item|
